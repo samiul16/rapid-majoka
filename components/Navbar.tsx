4db1d15e7367a1af092e8fde18f43b5666bf83de
pH0enix46 <mdjoy.javascript@gmail.com>
2025-11-05 20:49:15 +0600

    working

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -125,7 +125,7 @@ const Navbar = () => {
       style={{ zIndex: 9998 }}
     >
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-16">
-        <div className="flex items-center justify-between h-16 sm:h-20">
+        <div className="flex items-center justify-between h-16 sm:h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
             <Link href="/">
